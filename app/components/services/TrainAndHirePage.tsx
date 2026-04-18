dafb43a9febb15848416de662734cfdb4a1dd694
raghulvj01 <raghulvijay01@gmail.com>
2026-04-18 10:31:10 +0530

    feat: implement responsive navigation header and service pages for corporate training, train-and-hire, and freshers drive

--- app/components/services/TrainAndHirePage.tsx
+++ app/components/services/TrainAndHirePage.tsx
@@ -5,7 +5,6 @@ import { capabilitiesData } from "@/app/data/capabalitiesData";
 import ProcessSection from "./train-and-hire/ProcessSection";
 import WhySection from "./train-and-hire/WhySection";
 import TestimonialSection from "./train-and-hire/TestimonialSection";
-import CTASection from "./train-and-hire/CTASection";
 
 export default function TrainAndHirePage() {
   const trainAndHireData = capabilitiesData.items.find(
@@ -20,7 +19,6 @@ export default function TrainAndHirePage() {
       <ProcessSection />
       <WhySection />
       <TestimonialSection />
-      <CTASection />
     </div>
   );
 }

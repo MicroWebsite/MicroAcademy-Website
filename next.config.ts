67cf8479c620a119345a1f565ac4e66a5c55ba73
raghulvj01 <raghulvijay01@gmail.com>
2026-04-23 17:44:52 +0530

    feat: add local Strapi image provider configuration and rewrite rules to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -26,7 +26,21 @@ const nextConfig: NextConfig = {
         hostname: "168.144.70.136",
         pathname: "/sites/default/files/**",
       },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+        port: "1337",
+        pathname: "/uploads/**",
+      },
     ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/uploads/:path*",
+        destination: "http://127.0.0.1:1337/uploads/:path*",
+      },
+    ];
   },
   logging: {
     fetches: {
